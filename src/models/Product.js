9816c9584231e6e70a58b5dfffcb7867ec23677c
Juan Pablo Accinelli <juanpaccinelli@gmail.com>
2023-09-26 21:58:01 -0300

    Revert "Corregir tipos de datos en modelos"

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,48 +1,47 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('Product', {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-      },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      category1: {
-        //geek, movies, anime, wear
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      category2: {
-        //geek, movies, anime, wear
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      category3: {
-        //geek, movies, anime, wear
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      stock: {
-        //units available on inventary
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    });
+    sequelize.define("Product", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category1 : {   //geek, movies, anime, wear
+            type: DataTypes.STRING,
+            allowNull : true,
+        
+        },
+        category2 : {   //geek, movies, anime, wear
+            type: DataTypes.STRING,
+            allowNull : true,
+        
+        },
+        category3 : {   //geek, movies, anime, wear
+            type: DataTypes.STRING,
+            allowNull : true,
+        
+        },
+        stock: {  //units available on inventary
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+   )
 };
