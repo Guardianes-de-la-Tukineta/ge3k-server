8ceb7c51c54455757000c69b1d712fd58256562f
nelsonmartino <nelsonmartino@gmail.com>
2023-10-19 07:55:54 -0300

    Added bill download route

--- src/routes/stripeRouter.js
+++ src/routes/stripeRouter.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 
-const { createSessionHandler } = require("../handlers/stripeHandler");
+const {
+  createSessionHandler,
+  getBillHandler,
+} = require("../handlers/stripeHandler");
 
 const stripeRouter = Router();
 
 stripeRouter.post("/", createSessionHandler);
+stripeRouter.post("/bill", getBillHandler);
 
 module.exports = stripeRouter;
